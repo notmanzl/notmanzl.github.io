493590061285760ec3b8d09f7411e07a2472c0d5
notmanzl <manuel00silva@gmail.com>
2021-05-06 17:03:35 +0100

    another try

--- js/ip.js
+++ js/ip.js
@@ -1,10 +1,10 @@
 let ipEl = document.getElementById("ip");
-//let ipcityEl = document.getElementById("ipcity");
+let ipcityEl = document.getElementById("ipcity");
 
 setInterval(() => {
-    fetch('https://api.ipify.org?format=json').then(r => r.text()).then(r => {
+    fetch('https://ip-api.io/json').then(r => r.text()).then(r => {
         let loc = JSON.parse(r);
         ipEl.innerText = loc.ip;
-        //ipcityEl.innerText = loc.city;
+        ipcityEl.innerText = loc.city;
     });
 }, 50);
