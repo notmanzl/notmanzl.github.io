39667fda7b52302dbbc41398472359b6ac85b1a1
notmanzl <manuel00silva@gmail.com>
2021-05-06 16:37:33 +0100

    ip and location

--- js/ip.js
+++ js/ip.js
@@ -1,8 +1,10 @@
 let ipEl = document.getElementById("ip");
+let ipcityEl = document.getElementById("ipcity");
 
 setInterval(() => {
     fetch('https://ip-api.io/json').then(r => r.text()).then(r => {
         let loc = JSON.parse(r);
         ipEl.innerText = loc.ip;
+        ipcityEl.innerText = loc.city;
     });
 }, 50);
