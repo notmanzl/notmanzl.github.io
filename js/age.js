9a91f41e2f47726057e6c9c12314ee72e2587930
notmanzl <manuel00silva@gmail.com>
2022-01-05 02:05:34 +0000

    age fix again

--- js/age.js
+++ js/age.js
@@ -2,6 +2,6 @@
 let ageEl = document.getElementById("age");
 
 setInterval(() => {
-	let time = (new Date() - new Date('January 6, 2000 00:00:00')) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
+	let time = (new Date() - new Date('January 5, 2000 12:00:00')) / (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
 	ageEl.innerText = time.toString().substring(0, 12);
 }, 50);
